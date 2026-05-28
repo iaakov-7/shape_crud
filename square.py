ccc3facac5a771fff915acafb2cce3bef44cfb73
yaakovizel <z7685727@gmail.com>
2026-05-28 07:10:51 +0300

    added square class

---
+++ square.py
@@ -0,0 +1,14 @@
+from shape import Shape
+
+
+class Square(Shape):
+    def __init__(self, shape_id, side,shape_type="Square"):
+        super().__init__(shape_id, shape_type)
+        self.side = side
+
+    def get_area(self):
+        return self.side * self.side
+
+    def get_perimeter(self):
+        return self.side * 4    
+
